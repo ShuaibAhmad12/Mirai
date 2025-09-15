2752c977abf04552ce3791633ca7fbf9a2e1f7bd
kruxxtech <132448576+kruxxtech@users.noreply.github.com>
2025-09-15 10:46:33 +0530

    Delete database directory

--- database/Schema/006.3_remove_constraints_temp.sql
+++
@@ -1,5 +0,0 @@
--- Temporarily remove foreign key constraints to allow import
--- Run this before importing the CSV data
-
-ALTER TABLE fee_current_balances DROP CONSTRAINT IF EXISTS fk_fee_current_balances_fee_component;
-ALTER TABLE fee_current_balances DROP CONSTRAINT IF EXISTS fk_fee_current_balances_enrollment;
